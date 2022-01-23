7215d3b484f817ffa498fd5e1bca383fea0d4671
Saatvik Rao <76424133+saatvikrao@users.noreply.github.com>
2022-01-24 00:44:05 +0530

    Update py_int.cpp

--- Assignment - 1/Question 2/py_int.cpp
+++ Assignment - 1/Question 2/py_int.cpp
@@ -11,10 +11,11 @@ length_of_dic = dic[n]
 
 
 
-arr1 = [ [ rand.randint(0,100) for i in range(0, length_of_dic) ] for j in range(0, length_of_dic) ]
-arr2 = [ [ rand.randint(0,100) for k in range(0, length_of_dic) ] for l in range(0, length_of_dic) ]
+array1 = [ [ rand.randint(0,100) for i in range(0, length_of_dic) ] for j in range(0, length_of_dic) ]
+ 
+array2 = [ [ rand.randint(0,100) for k in range(0, length_of_dic) ] for l in range(0, length_of_dic) ]
 
-ans = [ [0 for p in range(0, length_of_dic)] for q in range(0, length_of_dic) ]
+array = [ [0 for p in range(0, length_of_dic)] for q in range(0, length_of_dic) ]
 
 
 
@@ -27,7 +28,7 @@ for u in range(0, length_of_dic):
     for v in range(0, length_of_dic):
         for w in range(0, length_of_dic):
 
-            ans[u][v] = ans[u][v] + ( arr1[u][w] * arr2[w][v] )
+            array[u][v] = array[u][v] + ( array1[u][w] * array2[w][v] )
 
               
         
