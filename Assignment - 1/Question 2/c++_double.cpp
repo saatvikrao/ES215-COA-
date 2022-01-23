e72accc98d4694d6f9d68f7f0418b8bac156d92d
Saatvik Rao <76424133+saatvikrao@users.noreply.github.com>
2022-01-24 00:17:33 +0530

    Create c++_double.cpp

---
+++ Assignment - 1/Question 2/c++_double.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <sys/time.h>
+
+using namespace std;
+
+struct timespec; time_t c1, c2;
+
+int main()
+{
+    c1 = clock();
+
+    srand(time(0));
+  
+    int n; cin >> n;
+    double arr1[n][n];
+    int arr2[n][n];
+    int arr_ans[n][n];
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            arr1[i][j] = (rand() % 5);
+        }
+    }
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            arr2[i][j] = (rand() % 5);
+        }
+    }
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            arr_ans[i][j] = 0;
+
+            for (int k = 0; k < n; k++){
+                arr_ans[i][j] += arr1[i][k] * arr2[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cout << arr_ans[i][j] << "\t";
+        }
+      
+        cout << "\n";
+    }
+
+    c2 = clock() - c1;
+  
+    float CPU_time = (float)c2/CLOCKS_PER_SEC;
+    
+    cout << "The time taken is: " << CPU_time << "\n";
+
+    return 0;
+}
